d494fdffdec7b76bd827881be31bc9cdd8205ab6
Vicky <v2chou@ucsd.edu>
2022-01-19 04:44:14 -0800

    added brief intro

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <Home/> 
+        {/* <span className="">🥐🥞🍦🍰</span> */}
       </header>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,15 @@ import "../css/Home.css";
 function Home() {
 
     return (
-        <div>
+        <div className="landing">
             <span className="name">Vicky Chou</span>
+            <div className="intro">
+                <p>Hello! I am a cognitive science student from UC San Diego ('23) and enjoy developing a meaningful user experience. Currently an REU web developer intern at Center for Applied Internet Data Analysis(CAIDA).</p>
+                <p>Besides school, work, and clubs, I love being a badminton, cookie butter, and corgi butt lover.</p>
+            </div>
+            <div className="experience">
+                Featured work under: CAIDA, CodePath, Latinas Contra Cancer
+            </div>
         </div>
     )
 }
