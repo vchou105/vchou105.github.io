540a3a1bbed93886acfaeedd458b3b586c46e4e9
Vicky <v2chou@ucsd.edu>
2022-01-19 03:55:55 -0800

    initial home page

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from "./components/Home.js";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <Home/> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import "../css/Home.css";
+
+function Home() {
+
+    return (
+        <div>
+            <span className="name">Vicky Chou</span>
+        </div>
+    )
+}
+
+export default Home;
