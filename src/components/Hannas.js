a144b0ed6d55e4af2dc81018d571f3bdd9f34a75
Vicky <v2chou@ucsd.edu>
2022-05-06 18:06:22 -0700

    added structure of hannas

--- src/components/Hannas.js
+++ src/components/Hannas.js
@@ -4,8 +4,21 @@ import HannasCreamery from '../img/HannasCreameryPreview.png';
 
 function Hannas() {
     return (
-        <div>
+        <div className="caseStudy">
             <img src={HannasCreamery} alt="Hanna's Creamery Project"/>
+            <section>
+                <h2>Overview</h2>
+                <h2>Introduction</h2>
+                <h2>Problem</h2>
+                <h2>Meeting Ali</h2>
+                <h2>Anyone heard of Hanna's Creamery?</h2>
+                <h2>What is the current state?</h2>
+                <h2>User Flows</h2>
+                <h2>Low-Fidelity Prototype</h2>
+                <h2>User Testing & Challenges</h2>
+                <h2>Hi-Fidelity Prototype</h2>
+                <h2>Reflections</h2>
+            </section>
         </div>
     )
 }
