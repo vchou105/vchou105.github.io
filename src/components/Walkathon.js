8ed3d6cb89179085599cc96929cf435349a584ec
Vicky <v2chou@ucsd.edu>
2022-05-07 16:19:50 -0700

    added hashrouter and virtual walkathon headings

--- src/components/Walkathon.js
+++ src/components/Walkathon.js
@@ -4,7 +4,24 @@ import VirtualWalkathon from '../img/VirtualWalkathonPreview.png';
 function Walkathon() {
     return (
         <div>
-            <img src={VirtualWalkathon} alt="Virtual Walkathon Project"/>
+            <img src={VirtualWalkathon} alt="Virtual Walkathon Project" />
+            <h2>Overview</h2>
+            <h2>Introduction</h2>
+            <h2>Problem</h2>
+            <h2>Solution</h2>
+            <h2>Background</h2>
+            <h2>So... How do virtual walkathons work?</h2>
+            <h2>Understanding our users</h2>
+            <h2>We learned to...</h2>
+            <h2>How exactly will our virtual walkathon work? 3-Day vs 1-Day Itineraries</h2>
+            <h3>3-Day Itinerary</h3>
+            <h3>1-Day Itinerary</h3>
+            <h3>Itinerary Feedback</h3>
+            <h2>Gotta make use of this big event to fundraise!</h2>
+            <h2>Mid-Fidelity Prototype</h2>
+            <h2>A/B Testing</h2>
+            <h2>Final Prototype</h2>
+            <h2>Reflection</h2>
         </div>
     )
 }
