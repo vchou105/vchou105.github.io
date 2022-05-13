31c113d97ac4eb7b750ee6851952409c30206b08
Vicky <v2chou@ucsd.edu>
2022-05-12 23:42:40 -0700

    added persona for hanna's and cropped family persona

--- src/components/Hannas.js
+++ src/components/Hannas.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import HannasCreamery from '../img/HannasCreameryPreview.png';
+import CateringPersona from '../img/HannasCreamery/CateringPersona.png';
+import FamilyPersona from '../img/HannasCreamery/FamilyPersona.png';
+import StudentPersona from '../img/HannasCreamery/StudentPersona.png';
+import MoodboardA from '../img/HannasCreamery/MoodboardA.png';
+import MoodboardB from '../img/HannasCreamery/MoodboardB.png';
+import Sitemap from '../img/HannasCreamery/Sitemap.png';
 
 
 function Hannas() {
     return (
         <div className="caseStudy">
-            <div className="caseStudy-hero">
+            <div className="img-100">
                 <img src={HannasCreamery} alt="Hanna's Creamery Project"/>
             </div>
             <section>
@@ -14,8 +20,16 @@ function Hannas() {
                 <h2>Problem</h2>
                 <h2>Meeting Ali</h2>
                 <h2>Anyone heard of Hanna's Creamery?</h2>
+                <div className='persona img-100'>
+                    <img src={StudentPersona} alt="Student persona"/>
+                    <img src={FamilyPersona} alt="Family persona"/>
+                    <img src={CateringPersona} alt="Catering persona"/>
+                </div>
                 <h2>What is the current state?</h2>
                 <h2>User Flows</h2>
+                <div className='img-100'>
+                    <img src={Sitemap} alt="Sitemap"/>
+                </div>
                 <h2>Low-Fidelity Prototype</h2>
                 <h2>User Testing & Challenges</h2>
                 <h2>Hi-Fidelity Prototype</h2>
