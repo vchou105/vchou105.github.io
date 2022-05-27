286f5b7b549de7a25a2b3d996f82d7dd0440fb80
Vicky <v2chou@ucsd.edu>
2022-05-27 04:13:05 -0700

    added walkathon mid fi ab prototypes

--- src/components/Walkathon.js
+++ src/components/Walkathon.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import VirtualWalkathon from '../img/VirtualWalkathonPreview.png';
+import prototypeA from '../img/VirtualWalkathon/Prototype A.svg';
+import prototypeB from '../img/VirtualWalkathon/Prototype B.svg';
 import finalPrototype from '../img/VirtualWalkathon/WalkathonFinal.png';
 import "../css/Project.css";
 
@@ -17,30 +19,30 @@ function Walkathon() {
                     <li>Oct - Dec 2020 (7 weeks)</li>
                 </ul>
             </div>
-            
+
             <h2>Introduction</h2>
             <div>
                 <p>Creating an inclusive health care system around cancer issues for the underserved Latino population, Latinas Contra Cancer is a non-profit organization that hosts an annual walkathon on Mother’s Day where the community walks <em>in memory for their loved ones, for equity, for health, and for each other</em>.</p>
             </div>
-            
+
             <h2>Problem</h2>
             <p>How might we design a virtual walkathon that fosters more support and awareness in our community?</p>
-            
+
             <h2>Solution</h2>
             <p>A one-day virtual event with workshops and speakers sharing topics on inequality, racism, and support to create a welcoming community while fundraising through registration packages.</p>
-            
+
             <h2>Background</h2>
-            
+
             <h2>So... How do virtual walkathons work?</h2>
             <p>With no clue to how virtual walkathons engage with participants, we referenced the few virtual events we were able to find, such as <i>UCSD’s Triton 5K</i> and <a href="https://www.baa.org/124th-boston-marathon-be-held-virtually"><i>Boston Marathon</i></a>, which was held through an app that allows participants to be updated with the marathon schedule and notices.</p>
-            
+
             <h2>Understanding our audience</h2>
             <p>To even understand people's experiences in <i>virtual environments</i>, we:</p>
             <ul>
                 <li>Surveyed <b>70+</b> respondents who have attended events in a virtual setting with ages ranging from <b>{"\<"} 18 to 50+</b></li>
                 <li>Interviewed <b>1 primary stakeholder</b>, Nora, who has attended the walkathon in the past and heavily involved in the organization</li>
             </ul>
-            
+
             <h2>We learned to...</h2>
             <ul>
                 <li>Focus on the <i>collectiveness of community</i> → getting to know and hear stories from the community + seeing the impact of the walk</li>
@@ -48,14 +50,14 @@ function Walkathon() {
                 <li>Incentives may encourage outside participation</li>
                 <li>Although virtual, want to still feel a sense of community and interaction with others</li>
             </ul>
-            
+
             <h2>How exactly will our virtual walkathon work? 3-Day vs 1-Day Itineraries</h2>
             <p>To design a virtual walkathon that allows our target audience to make the most out of this opportunity while feeling a sense of community, we ideated a 3-day version and a 1-day version event</p>
             <h3>3-Day Itinerary</h3>
             <h3>1-Day Itinerary</h3>
-            
+
             <h3>Itinerary Feedback</h3>
-            
+
             <h2>Gotta make use of this big event to fundraise!</h2>
             <p>In light of our research takeaways, we wanted to raise awareness of LCC’s mission while raising participation incentives through:</p>
             <ul>
@@ -69,6 +71,16 @@ function Walkathon() {
                 <li>We each wireframed our own versions of the virtual walkathon page and consolidated features and layouts we liked from our prototypes to compose a slightly more polished mid-fidelity prototype</li>
                 <li>Since we weren’t sure what combination of features work better, we made two different versions to test out</li>
             </ul>
+            <div className='ab'>
+                <div id='prototypeA'>
+                    <h3>Prototype A</h3>
+                    <img src={prototypeA} className="" alt="Virtual Walkathon Protoytpe A" />
+                </div>
+                <div id='prototypeB'>
+                    <h3>Prototype B</h3>
+                    <img src={prototypeB} className="" alt="Virtual Walkathon Protoytpe B" />
+                </div>
+            </div>
             <a></a>
 
             <h2>A/B Testing</h2>
@@ -81,7 +93,7 @@ function Walkathon() {
 
             <h2>Final Prototype</h2>
             <img src={finalPrototype} alt="Virtual Walkathon Website Final Prototype" />
-            
+
             <h2>Reflection</h2>
             <ul>
                 <li>This was my first exposure to the design process and I had previously not known A/B testing was even a thing! It was very cool testing the comparisons and I had a lot of fun seeing what user testing is actually like!</li>
